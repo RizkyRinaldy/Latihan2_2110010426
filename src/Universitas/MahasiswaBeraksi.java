d9e03d1666b30425f3ee2d37fc9dfb5ef77e6220
RizkyRinaldy <rizky3rinaldy@gmail.com>
2023-06-13 09:47:32 +0800

    Menjalankan Program

--- src/Universitas/MahasiswaBeraksi.java
+++ src/Universitas/MahasiswaBeraksi.java
@@ -1,5 +1,12 @@
 package Universitas;
 
 public class MahasiswaBeraksi {
-    String npm,nama,prodi;
+    public static void main(String[] args) {
+        Mahasiswa data = new Mahasiswa("2110010426","Rinaldy","TI");
+        
+        System.out.println("NPM ="+data.getNpm());
+        System.out.println("Nama ="+data.getNama());
+        System.out.println("Prodi ="+data.getProdi());
+        
+    }
 }
